bea4a26e27de858a547481890ddc0628dde7853b
Henrique <rickdbs.dev@hotmail.com>
2023-05-24 09:27:24 -0300

    002 - Finalização da página Home

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -9,7 +9,8 @@ export const Container = styled.section`
         border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
         
         padding-bottom: 16px;
-
+        margin-bottom: 24px;
+        
         color: ${({ theme }) => theme.COLORS.GRAY_100};
         font-size: 20px;
         font-weight: 400;
